2f9f8fc953819e999dd4a593b0151c23cd0fdbb7
jeffjacksonjr <jeffjacksonw59@gmail.com>
2025-07-16 00:16:57 +0530

    JWT Security implemented

--- src/main/java/com/jeffjackson/config/CorsConfig.java
+++ src/main/java/com/jeffjackson/config/CorsConfig.java
@@ -6,12 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://www.jeffjacksonjr.com","https://jeffjacksonjr.netlify.app", "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowedHeaders("*");
+                .allowedOrigins("https://www.jeffjacksonjr.com",
+                        "https://jeffjacksonjr.netlify.app",
+                        "http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization");
     }
 }
---
+++ src/main/java/com/jeffjackson/security/model/User.java
@@ -0,0 +1,64 @@
+package com.jeffjackson.security.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Document(collection = "users")
+public class User implements UserDetails {
+    @Id
+    private String id;
+    private String email;
+    private String password;
+    private Role role;
+
+    // Constructors, getters, setters
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override public String getUsername() { return email; }
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
